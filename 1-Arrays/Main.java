69a455c714f2c7e5e038c327577cddad70c90290
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-09 07:00:43 +0530

    Declaring an Array

---
+++ 1-Arrays/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main {
+	public static void main (String[] args) throws java.lang.Exception	{
+		// your code goes here
+		Scanner scan = new Scanner(System.in);
+		
+		// creating an array
+		int[] arr = new int[5];
+		int[] arr2 = {1,2,3,4,5};
+		
+	}
+}
