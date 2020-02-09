b285717ed24f1745984ab4c4c8212f22425c9b32
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-09 07:19:00 +0530

    Code 1d Array and using common operations

--- 1-Arrays/Main.java
+++ 1-Arrays/Main.java
@@ -12,5 +12,26 @@ public class Main {
 		int[] arr = new int[5];
 		int[] arr2 = {1,2,3,4,5};
 		
+		// inputting the array
+		for(int i=0;i<5;i++)
+		    arr[i] = scan.nextInt();
+		
+		// Code 1d Array and using common operations
+		try{
+		    // traverse the array
+		    for(int i=0; i<5;i++)
+		        System.out.println(arr[i]);
+		    
+		    // insert the value
+		    arr[2] = 5; // this is the new value
+		    
+		    // search the array using value and return cell index
+		    for(int i=0;i<5;i++){
+		        if(arr[i] == 3) // where 3 is the value we need to find
+		            System.out.println("Found at index: " + i);
+		        else
+		            System.out.println("Not present");
+		    }
+		}catch(Exception e){}	
 	}
 }
