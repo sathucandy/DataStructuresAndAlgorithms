37686d70e742073a63ad1c1de4768b71258ea29c
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-21 04:21:48 +0530

    Adding push(), pop(), peek(), isEmpty() and show() method

--- 3-Stack/Main.java
+++ 3-Stack/Main.java
@@ -2,6 +2,20 @@ import java.util.*;
 import java.lang.*;
 public class Main {
     public static void main(String[] args) {
+        Stack nums = new Stack();
+        nums.push(15);
+        nums.push(10);
         
+        System.out.println(nums.peek());
+        
+        nums.push(5);
+        
+        System.out.println(nums.pop());
+        
+        System.out.println("Size is: " + nums.size());
+        
+        System.out.println("If the size is empty or not: " + nums.isEmpty());
+        
+        nums.show();
     }
 }
--- 3-Stack/Stack.java
+++ 3-Stack/Stack.java
@@ -1,5 +1,40 @@
 public class Stack {
-    public static void main(String[] args) {
-        System.out.println("give me a bottle of rum!");
+
+    int[] stack = new int[5];
+    int top = 0;
+    
+    public void push(int data){
+        stack[top] = data;
+        top++;
+    }   
+    
+    public int pop(){
+        int data;
+        top--;
+        data = stack[top];
+        stack[top] = 0;
+        return data;
+    } 
+    
+    public int peek(){
+        int data;
+        data = stack[top-1];
+        stack[top] = 0;
+        return data;
+    } 
+    
+    public int size(){
+        return top;
     }
+    
+    public boolean isEmpty(){
+        return top<=0;
+    }
+    
+    public void show(){
+        for(int n: stack){
+            System.out.println(n + " ");
+        }
+    }
+    
 }
